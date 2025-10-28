df35ab9e9989a0c7c0d74e0feab84d2d4f800961
minjunsong <songmj5000@naver.com>
2025-10-28 17:16:18 +0900

    큐 - 2025.10.28

---
+++ baekjoon/BOJ_2164.java
@@ -0,0 +1,24 @@
+package baekjoon;
+
+import java.io.*;
+import java.util.*;
+
+public class BOJ_2164 {
+  
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Queue<Integer> q = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
+        }
+
+        while (q.size() > 1) {
+            q.poll();
+            q.offer(q.poll());
+        }
+
+        System.out.print(q.poll());
+    }
+}
