7ca564ebc00b7b419aaab255567c9768160cad1f
Minjun Song <157129896+Minjun1023@users.noreply.github.com>
2026-01-25 15:27:29 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2524 ms, Memory: 358140 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,66 +2,46 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    
-    public static void clean(PriorityQueue<Integer> pq, Map<Integer, Integer> map) {
-        while (!pq.isEmpty()) {
-            int x = pq.peek();
-            if (map.getOrDefault(x, 0) == 0) {
-                pq.poll();
-            } else {
-                break;
-            }
-        }
-    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int t = Integer.parseInt(br.readLine());
-    
-        while (t --> 0) {
-            int k = Integer.parseInt(br.readLine());
 
-            PriorityQueue<Integer> minPQ = new PriorityQueue<>();
-            PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Collections.reverseOrder());
-            Map<Integer, Integer> map = new HashMap<>();
-            for (int i = 0; i < k; i++) {
+        while (t --> 0) {
+            int n = Integer.parseInt(br.readLine());
+            TreeMap<Integer, Integer> map = new TreeMap<>();
+
+            for (int i = 0; i < n; i++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
                 char c = st.nextToken().charAt(0);
                 int num = Integer.parseInt(st.nextToken());
 
                 if (c == 'I') {
-                    minPQ.add(num);
-                    maxPQ.add(num);
-                    map.put(num, map.getOrDefault(num, 0) + 1);
-                } else {    
+                    map.put(num, map.getOrDefault(num, 0) + 1); 
+                } else {
                     if (map.isEmpty()) continue;
-
+                    
                     if (num == 1) {
-                        clean(maxPQ, map);
-                        if (!maxPQ.isEmpty()) {
-                            int x = maxPQ.poll();
-                            map.put(x, map.get(x) - 1);
-                            if (map.get(x) == 0) map.remove(x);
+                        int max = map.lastKey();
+                        if (map.put(max, map.get(max) - 1) == 1) {
+                            map.remove(max);
                         }
                     } else {
-                        clean(minPQ, map);
-                        if (!minPQ.isEmpty()) {
-                            int x = minPQ.poll();
-                            map.put(x, map.get(x) - 1);
-                            if (map.get(x) == 0) map.remove(x);
+                        int min = map.firstKey();
+                        if (map.put(min, map.get(min) - 1) == 1) {
+                            map.remove(min);
                         }
                     }
-                }     
+                }
             }
-
             if (map.isEmpty()) {
                 sb.append("EMPTY").append('\n');
             } else {
-                clean(maxPQ, map);
-                clean(minPQ, map);
-                sb.append(maxPQ.peek()).append(" ").append(minPQ.peek()).append('\n');
+                sb.append(map.lastKey()).append(" ").append(map.firstKey()).append('\n');
             }
         }
         System.out.print(sb);
     }
+    
 }
