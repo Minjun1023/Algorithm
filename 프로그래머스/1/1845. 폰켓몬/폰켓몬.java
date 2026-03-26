2ba1aba8893c58fdf50c9105f9a19974a33ead19
Minjun Song <157129896+Minjun1023@users.noreply.github.com>
2026-03-26 13:50:15 +0900

    [level 1] Title: 폰켓몬, Time: 4.80 ms, Memory: 77.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] nums) {
+        int answer = nums.length / 2;
+        Set<Integer> set = new HashSet<>();
+        
+        for (int i : nums) {
+            set.add(i);
+        }
+        
+        if (answer >= set.size()) {
+            return set.size();
+        } else {
+            return answer;
+        }
+    }
+}
